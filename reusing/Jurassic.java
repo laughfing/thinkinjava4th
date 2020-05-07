b6897457a64d06f75212f670cdd9e6ca8d6c31a6
laughfing <125116454@qq.com>
2020-05-07 11:08:46 +0800

    2020571108

---
+++ reusing/Jurassic.java
@@ -0,0 +1,16 @@
+package reusing;
+
+class SmallBrain{
+
+}
+
+final class Dinosaur {
+    int i = 7;
+    int j = 1;
+    SmallBrain x = new SmallBrain();
+    void f() {}
+}
+
+public class Jurassic {
+    
+}
