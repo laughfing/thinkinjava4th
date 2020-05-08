789aa454b0007b0b68e73b50d0a8cea37cd344aa
laughfing <125116454@qq.com>
2020-05-08 09:33:02 +0800

    202058932

---
+++ polymorphism/music/Instrument.java
@@ -0,0 +1,7 @@
+package polymorphism.music;
+
+class Instrument {
+    public void play(Note e) {
+        System.out.println("Instrument.play()");
+    }    
+}
---
+++ polymorphism/shape/Shapes.java
@@ -0,0 +1,15 @@
+package polymorphism.shape;
+
+public class Shapes {
+    private static RandomShapeGenerator gen = new RandomShapeGenerator();
+    public static void main(String[] args) {
+        Shape[] s = new Shape[9];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = gen.Next();
+        }
+
+        for (Shape shp : s) {
+            shp.draw();
+        }
+    }
+}
