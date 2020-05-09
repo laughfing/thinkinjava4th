96a02d47098b6e6d79feced157a3536c0722d3a2
laughfing <125116454@qq.com>
2020-05-09 09:52:28 +0800

    202059952

---
+++ interfaces/interfaceprocessor/StringProcessor.java
@@ -0,0 +1,40 @@
+package interfaces.interfaceprocessor;
+
+import java.util.Arrays;
+
+class Upcase extends StringProcessor {
+
+
+	public Object process(Object input) {
+		return ((String)input).toUpperCase();
+	}
+    
+}
+
+class Downcase extends StringProcessor {
+    public String process(Object input) {
+    return ((String)input).toLowerCase();
+    }
+}
+
+class Splitter extends StringProcessor {
+    public String process(Object input) {
+    return Arrays.toString(((String)input).split(" "));
+    }
+}
+public abstract class StringProcessor implements Processor{
+
+    public String name() {
+        return getClass().getSimpleName();
+    }
+
+    public abstract Object process(Object input);
+
+    public static String s = "If she weighs the same as a duck, she’s made of wood";
+
+    public static void main(String[] args) {
+        Apply.process(new Upcase(), s);
+        Apply.process(new Downcase(), s);
+        Apply.process(new Splitter(), s);
+    }   
+}
