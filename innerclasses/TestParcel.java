185a043d2a8392bd52976e3ec475b86a2a862f1a
laughfing <125116454@qq.com>
2020-05-12 11:07:53 +0800

    20205121107

---
+++ innerclasses/TestParcel.java
@@ -0,0 +1,35 @@
+package innerclasses;
+
+class Parcel4 {
+    private class PContents implements Contents {
+        private int i = 11;
+        public int value() {
+            return i;
+        }
+    }
+
+    protected class PDestination implements Destination {
+        private String label;
+        private PDestination(String whereTo){
+            label = whereTo;
+        }
+        public String readLabel() {
+            return label;
+        }
+        
+    }
+
+    public Destination destination(String s){
+        return new PDestination(s);
+    }
+
+    public Contents contents(){
+        return new PContents();
+    }
+}
+
+public class TestParcel {
+    Parcel4 p = new Parcel4();
+    Contents c = p.contents();
+    Destination d = p.destination("Tasmania");
+}
